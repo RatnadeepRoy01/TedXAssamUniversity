6a1ca49c84952c8a19f06c3b4074968cee8ba14b
Ratnadeep Roy <ratnadeep30112005@gmail.com>
2025-07-14 01:01:04 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "TEDx Assam University",
+  icons: {
+    icon: "/logo1.ico",
+  },
   description: "Official website for TEDx Assam University. Discover speakers, schedule, team, and more.",
 };
 
